3886c1e7933ddd4432e96fba9506f01839d1db3e
samuelmaddock <samuel.maddock@gmail.com>
2013-06-27 20:25:16 -0400

    Removed debug message from docRainbow directive

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,6 @@ angular.module('docsApp', ['docsApp.controllers'])
 	.directive('docRainbow', function() {
 		return function(scope, element, attrs) {
 			if ( scope.$last && window.Rainbow ) {
-				console.log("LAST ELEM");
 				setTimeout(function(){
 					Rainbow.color();
 				}, 100);
