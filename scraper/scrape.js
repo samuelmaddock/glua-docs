bf753149c1c1e8057a516f1641d4cb04b8cef5a5
samuelmaddock <samuel.maddock@gmail.com>
2013-06-27 20:26:41 -0400

    Cleaned up scraper a bit

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,97 +1,83 @@
+// Get command line args
+var args = process.argv.splice(2);
+args.forEach(function(val, index, array) {
+	console.log(index + ': ' + val);
+});
+
 var requirejs = require('requirejs');
 
 requirejs([
-  'spider',
-  'underscore',
-  'cheerio',
-  'path',
-  'fs'
+	'spider',
+	'underscore',
+	'cheerio',
+	'path',
+	'fs'
 ], function(spider, _, cheerio, path, fs) {
 
-  var domain = 'wiki.garrysmod.com';
-  var scrapeData = [];
-  var spidey = spider({
-    userAgent: "samuel.maddock@gmail.com"
-  });
+	var domain = 'wiki.garrysmod.com';
+	var luaStates = ['server','shared','client','menu'];
+	var rootUrls = [
+		'http://wiki.garrysmod.com/page/Category:Hooks',
+		'http://wiki.garrysmod.com/page/Category:Functions',
+		//'http://wiki.garrysmod.com/page/Enums'
+	];
 
-  // file where we'll dump the json
-  var filename = path.dirname(__filename) + '/../app/data/glua.json';
-  console.log('[Dumping to ' + filename + '.]');
-  var file = fs.openSync(filename, 'w');
+	var scrapeData = [];
+	var spidey = spider({
+		userAgent: "GluaDocs Bot"
+	});
 
-  var rootUrls = [
-    'http://wiki.garrysmod.com/page/Category:Hooks',
-    'http://wiki.garrysmod.com/page/Category:Functions',
-    //'http://wiki.garrysmod.com/page/Enums'
-  ];
+	// file where we'll dump the json
+	var filename = path.dirname(__filename) + '/../app/data/glua.json';
+	console.log('[Dumping to ' + filename + '.]');
+	var file = fs.openSync(filename, 'w');
 
-  var scrapeCount = 0;
+	spidey.route(domain, '/page/Category:Hooks', function ($, url) {
+		console.log('---------');
+		console.log('scraping:', url);
 
-  spidey.route(domain, '/page/Category:Hooks', function ($, url) {
-    console.log('---------');
-    console.log('scraping:', url);
+		var links = $('table ul li a');
 
-    var links = $('table ul li a');
+		_.each(links, function(elt) {
+			spidey.get('http://' + domain + $(elt).attr('href'));
+		});
+	});
 
-    _.each(links, function(elt) {
-      // only scrape a few pages for testing
-      // if ( scrapeCount > 10 ) return;
-      // scrapeCount++;
-      spidey.get('http://' + domain + $(elt).attr('href'));
-    });
-  });
+	spidey.route(domain, '/page/*/*', function ($, url) {
+		console.log('glua scraping:', url);
 
-  spidey.route(domain, '/page/*/*', function ($, url) {
-    console.log('glua scraping:', url);
+		var catlinks = $('#mw-normal-catlinks').html().toLowerCase();
 
-    /*var sectionNames = [],
-      sectionHTMLs = [];
+		// Determine Lua state/realm
+		var scope = '';
+		for(var i = 0; i < luaStates.length; i++) {
+			if ( catlinks.indexOf(luaStates[i]) > 0 ) {
+				scope = luaStates[i];
+				break;
+			}
+		}
 
-    // function usage
-    sectionNames.push('Usage');
-    sectionHTMLs.push($('.function_line').text());
+		var title = $('head').html().match(/"wgPageName": "(\S+)"/)[1]
+			.replace('/','.');
 
-    // Get section names
-    $('#bodyContent .mw-headline').each(function(i,elem){
-      sectionNames.push($(elem).html());
-    });
+		scrapeData.push({
+			url   : url,
+			title : title,
+			html  : $('#bodyContent .mw-content-ltr').html(),
+			scope : scope
+		});
+	});
 
-    // Get section contents
-    $('#bodyContent .mw-content-ltr h1 + *').each(function(i,elem){
-      sectionHTMLs.push($(elem).html());
-    });*/
+	// Start 'er up
+	_.each(rootUrls, function(url) {
+		spidey.get(url).log('info');
+	});
 
-    var scope = '';
-    var catlinks = $('#mw-normal-catlinks').html();
+	process.on('exit', function () {
+		fs.writeSync(file, JSON.stringify(scrapeData, null, '\t'));
+		console.log('DONE');
+	});
 
-    if ( catlinks.indexOf('Server') > 0 ) {
-      scope = 'server';
-    } else if ( catlinks.indexOf('Client') > 0 ) {
-      scope = 'client';
-    } else if ( catlinks.indexOf('Shared') > 0 ) {
-      scope = 'shared';
-    } else if ( catlinks.indexOf('Menu') > 0 ) {
-      scope = 'menu';
-    }
-
-    scrapeData.push({
-      url   : url,
-      title : $('#firstHeading').text().replace('/','.'),
-      html  : $('#bodyContent .mw-content-ltr').html(),
-      scope : scope
-    });
-  });
-
-  // Start 'er up
-  _.each(rootUrls, function(url) {
-    spidey.get(url).log('info');
-  });
-
-  process.on('exit', function () {
-    fs.writeSync(file, JSON.stringify(scrapeData, null, '\t'));
-    console.log('DONE');
-  });
-
-  return;
+	return;
 });
 
