ff2933180aa7127219ea87b6a323602ad61e7cb9
samuelmaddock <samuel.maddock@gmail.com>
2013-06-29 22:56:27 -0400

    Moved directives to its own file

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,3 @@
 'use strict';
 
-angular.module('docsApp', ['docsApp.controllers'])
-	.directive('docRainbow', function() {
-		return function(scope, element, attrs) {
-			if ( scope.$last && window.Rainbow ) {
-				setTimeout(function(){
-					Rainbow.color();
-				}, 100);
-			}
-		};
-	});
+angular.module('docsApp', ['docsApp.controllers','docsApp.directives']);
