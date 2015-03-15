91ffb3476567ba438790dd0f79e6f57166a41757
samuelmaddock <samuel.maddock@gmail.com>
2015-03-15 18:52:55 -0400

    Updated scraper to work for recent wiki changes.

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,123 +1,181 @@
-// Get command line args
-var args = process.argv.splice(2);
-args.forEach(function(val, index, array) {
-	console.log(index + ': ' + val);
+var fs = require('fs');
+var path = require('path');
+
+var async = require('async');
+var cheerio = require('cheerio');
+var request = require('request').defaults({
+	headers: {
+		'User-Agent': 'GLuaDocs Bot'
+	}
 });
 
-var requirejs = require('requirejs');
+var DOMAIN_NAME = 'wiki.garrysmod.com';
+var ROOT_PAGES = [
+	'http://wiki.garrysmod.com/page/Category:Hooks',
+	'http://wiki.garrysmod.com/page/Category:Functions'
+];
+var LUA_STATES = ['server', 'shared', 'client', 'menu'];
+var REQUEST_LIMIT = 30;
 
-requirejs([
-	'spider',
-	'underscore',
-	'cheerio',
-	'path',
-	'fs'
-], function(spider, _, cheerio, path, fs) {
+function fetchWikiEntries () {
+	var tasks = [];
 
-	var domain = 'wiki.garrysmod.com';
-	var luaStates = ['server','shared','client','menu'];
-	var rootUrls = [
-		'http://wiki.garrysmod.com/page/Category:Hooks',
-		'http://wiki.garrysmod.com/page/Category:Functions',
-		//'http://wiki.garrysmod.com/page/Enums'
-	];
+	for (var i = 0; i < ROOT_PAGES.length; i++) {
+		var rootUrl = ROOT_PAGES[i];
 
-	var scrapeData = [];
-	var spidey = spider({
-		userAgent: "GLuaDocs Bot"
+		var task = function (callback) {
+			request(rootUrl, function (err, resp, html) {
+				if (err) {
+					callback('Couldn\'t fetch entry list at ' + rootUrl);
+					return;
+				}
+
+				var $ = cheerio.load(html);
+				processEntryList($, rootUrl, callback);
+			});
+		};
+
+		tasks.push(task);
+	}
+
+	async.series(tasks, function (err, results) {
+		if (err) {
+			console.error('An error occured while scraping the wiki: ', err);
+		}
+
+		var data = results[0];
+
+		for (var i = 1; i < results.length; i++) {
+			data.concat(results[i]);
+		}
+
+		outputScrapeData(data);
+	});
+}
+
+function processEntryList ($, url, callback) {
+	console.log('Processing \'' + url + '\'..');
+
+	var entries = [];
+
+	$('table ul li a').each(function (i, elem) {
+		var href = $(elem).attr('href');
+		var url = 'http://' + DOMAIN_NAME + href;
+		entries.push(url);
 	});
 
-	// file where we'll dump the json
-	var filename = path.dirname(__filename) + '/../app/data/glua.json';
-	console.log('[Dumping to ' + filename + '.]');
-	var file = fs.openSync(filename, 'w');
-
-	spidey.route(domain, '/page/Category:Hooks', function ($, url) {
-		console.log('scraping:', url);
-
-		var links = $('table ul li a');
-
-		_.each(links, function(elt) {
-			spidey.get('http://' + domain + $(elt).attr('href'));
-		});
+	async.mapLimit(entries, REQUEST_LIMIT, fetchWikiEntry, function (err, results) {
+		console.log('Finished processing \'' + url + '\'.');
+		callback(null, results);
 	});
+}
 
-	spidey.route(domain, '/page/*/*', function ($, url) {
-		console.log('wiki scraping:', url);
-
-		var $catlinks = $('#mw-normal-catlinks')
-		var catlinks = $catlinks.html();
-
-		if (!catlinks) {
-			console.error('\tWARNING: Category links are missing on this page and it will thus be ignored.');
-			console.info($catlinks);
+function fetchWikiEntry (url, callback) {
+	request(url, function (err, resp, html) {
+		if (err) {
+			console.error('ERROR: ', err);
+			callback('Couldn\'t fetch entry at ' + url);
 			return;
 		}
 
-		catlinks = catlinks.toLowerCase();
+		var $ = cheerio.load(html);
+		processWikiEntry($, url, callback);
+	});
+}
 
-		// Determine Lua state/realm
-		var scope = '';
-		for(var i = 0; i < luaStates.length; i++) {
-			if ( catlinks.indexOf(luaStates[i]) > 0 ) {
-				scope = luaStates[i];
-				break;
-			}
+function processWikiEntry ($, url, callback) {
+	console.log('wiki scraping:', url);
+
+	var $catlinks = $('#mw-normal-catlinks')
+	var catlinks = $catlinks.html();
+
+	if (!catlinks) {
+		console.error('\tWARNING: Category links are missing on this page and it will thus be ignored.');
+		console.info($catlinks);
+		return;
+	}
+
+	catlinks = catlinks.toLowerCase();
+
+	// Determine Lua state/realm
+	var scope = '';
+	for(var i = 0; i < LUA_STATES.length; i++) {
+		if ( catlinks.indexOf(LUA_STATES[i]) > 0 ) {
+			scope = LUA_STATES[i];
+			break;
 		}
+	}
 
-		var title = $('head').html().match(/"wgPageName": "(\S+)"/)[1]
-			.replace('/','.');
+	var title = $('head').html().match(/"wgPageName": "(\S+)"/)[1]
+		.replace('/','.');
 
-		// Set links to absolute urls
-		function setAbsoluteUrl(idx, elem) {
-			elem.attribs.href = "http://" + domain + elem.attribs.href;
-			elem.attribs.target = "_blank";
-		}
-		$("#bodyContent .mw-content-ltr a[href^='/page/']").each(setAbsoluteUrl);
-		$("#bodyContent .mw-content-ltr a[href^='/index.php']").each(setAbsoluteUrl);
+	// Set links to absolute urls
+	function setAbsoluteUrl(idx, elem) {
+		elem.attribs.href = "http://" + DOMAIN_NAME + elem.attribs.href;
+		elem.attribs.target = "_blank";
+	}
 
-		// Wiki code processing
-		$("#bodyContent pre").each(function(i, e) {
-			var html = $(e).html();
-			html = html.replace(/if /g, 'if (');
-			html = html.replace(/if \(\(/g, 'if (');
-			html = html.replace(/ then/g, ') then');
-			html = html.replace(/\)\) then/g, ') then');
-			html = html.replace(/}\n/g, '}\n\n');
-			html = html.replace(/\)/g, ' )');
-			html = html.replace(/  \)/g, ' )');
-			html = html.replace(/\(/g, '( ');
-			html = html.replace(/\(  /g, '( ');
-			html = html.replace(/\( \)/g, '()');
-			html = html.replace(/\= \{\n/g, '=\n{\n');
-			$(e).html(html);
-		}).attr("data-language", "lua");
+	var $content = $('#bodyContent .mw-content-ltr');
 
-		// Insert args into function usage
-		var args = [];
-		var chunks = $(".arg_chunk").each(function(i,elem){
-			args.push($(elem).html());
-		});
-		$('.function_args').html( args.join(', ') );
+	$content.find("a[href^='/page/']").each(setAbsoluteUrl);
+	$content.find("a[href^='/index.php']").each(setAbsoluteUrl);
 
-		scrapeData.push({
-			url   : url,
-			title : title,
-			html  : $('#bodyContent .mw-content-ltr').html(),
-			scope : scope
-		});
+	// Wiki code processing
+	$content.find("pre").each(function(i, e) {
+		var html = $(e).html();
+		html = html.replace(/if /g, 'if (');
+		html = html.replace(/if \(\(/g, 'if (');
+		html = html.replace(/ then/g, ') then');
+		html = html.replace(/\)\) then/g, ') then');
+		html = html.replace(/}\n/g, '}\n\n');
+		html = html.replace(/\)/g, ' )');
+		html = html.replace(/  \)/g, ' )');
+		html = html.replace(/\(/g, '( ');
+		html = html.replace(/\(  /g, '( ');
+		html = html.replace(/\( \)/g, '()');
+		html = html.replace(/\= \{\n/g, '=\n{\n');
+		$(e).html(html);
+	}).attr("data-language", "lua");
+
+	// Insert args into function usage
+	var args = [];
+	var chunks = $content.find(".arg_chunk").each(function(i,elem){
+		args.push($(elem).html());
 	});
+	$content.find(".function_args").html( args.join(', ') );
 
-	// Start 'er up
-	_.each(rootUrls, function(url) {
-		spidey.get(url).log('info');
-	});
+	// Remove 'View Source' link since we already have this feature
+	var $viewSourceNode = $content.find('.external.text');
+	if ($viewSourceNode.length > 0) {
+		$viewSourceNode.parent().remove();
+	}
 
-	process.on('exit', function () {
-		fs.writeSync(file, JSON.stringify(scrapeData));
-		console.log('DONE');
-	});
+	var entryData = {
+		url: url,
+		title: title,
+		html: $('#bodyContent .mw-content-ltr').html(),
+		scope: scope
+	};
+	callback(null, entryData);
+}
 
-	return;
-});
+var OUTPUT_FILE;
 
+function outputScrapeData (data) {
+	console.info('Outputting JSON data..');
+
+	var json = JSON.stringify(data);
+	fs.writeSync(OUTPUT_FILE, json);
+
+	console.log('DONE');
+}
+
+function start () {
+	// attempt to open file where we will be outputting data
+	var filename = path.dirname(__filename) + '/../app/data/glua.json';
+	OUTPUT_FILE = fs.openSync(filename, 'w');
+
+	fetchWikiEntries();
+}
+
+start();
