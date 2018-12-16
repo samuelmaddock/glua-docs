2f688f60d15e0dcd47c49c5942f5e69dc011ecad
samuelmaddock <samuel.maddock@gmail.com>
2018-12-15 21:23:52 -0500

    Update deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,6 @@ module.exports = function (grunt) {
 	// Load grunt tasks automatically
 	require('load-grunt-tasks')(grunt);
 
-	// Time how long tasks take. Can help when optimizing build times
-	require('time-grunt')(grunt);
-
 	// Configurable paths
 	var config = {
 		app: 'app',
